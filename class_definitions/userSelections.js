8988fcedf6290dabf941262a237d8750d9508167
Dan Bax <Dan.D.Bax@gmail.com>
2023-02-18 13:09:51 -0600

    wip

--- class_definitions/userSelections.js
+++ class_definitions/userSelections.js
@@ -35,7 +35,6 @@ class UserSelections{
   };
 
   viewQuery(){
-    console.log(this);
     printUserSelectionsQueryOutput(this);
   }
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // GUI is not built yet, so use pre-defined User Settings:
 const User = require('./class_definitions/user');
 
-const user = new User(2,"Dan Bax","316 Heather Mill Dr.","Dan.D.Bax@gmail.com","(314) 406-5730");
+const user = new User(1,"Dan Bax","316 Heather Mill Dr.","Dan.D.Bax@gmail.com","(314) 406-5730");
 
 // TODO: In production create function to getUserSelections from event loop:
 const {buildUserSelections} = require('./temp/testSelections');

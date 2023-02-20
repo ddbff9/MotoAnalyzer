c3ece42b525c97d668ed9e94122ccabcf701052f
Dan Bax <Dan.D.Bax@gmail.com>
2023-02-20 01:04:42 -0600

    create CRUD for events on server.js

---
+++ routes/event.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const mysqlConnection = require('../utils/database');
+
+const Router = express.Router();
+
+Router.get('/', (req, res) => {
+  mysqlConnection.query(
+    "SELECT * FROM Event",
+    (err, results, fields) => {
+      if(!err) {
+        res.send(results);
+      } else {
+        console.log(err);
+      }
+    }
+  )
+});
+Router.post("/1", (req, res) => {
+  console.log(req.body.Id)
+});
+
+Router.post("/", (req, res) => {
+  console.log(req.body)
+  const event = req.body;
+  const sql = "SET @Id = ?;SET @Ama_id = ?;SET @Type = ?;SET @Name = ?;SET @Region = ?;SET @Venue_ID = ?;SET @Round_Number = ?;SET @Triple_Crown = ?;SET @Gate_Drop = ?;SET @Whoop_Section = ?;SET @Sand_Section = ?;SET @Soil_Id = ?; CALL Add_or_Update_Event(@Id, @Ama_id, @Type, @Name, @Region, @Venue_ID, @Round_Number, @Triple_Crown, @Gate_Drop, @Whoop_Section, @Sand_Section, @Soil_Id);";
+  mysqlConnection.query(sql,
+  [
+    event.Id,
+    event.Ama_id,
+    event.Type,
+    event.Name,
+    event.Region,
+    event.Venue_ID,
+    event.Round_Number,
+    event.Triple_Crown,
+    event.Gate_Drop,
+    event.Whoop_Section,
+    event.Sand_Section,
+    event.Soil_Id,
+  ],
+    (err, results, fields) => {
+      if (!err) {
+        results.forEach((element) => {
+          if (element.constructor == Array) res.send(element);
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+Router.put("/", (req, res) => {
+  let event = req.body;
+  const sql = 
+     "SET @Id = ?; SET @Ama_id = ?; SET @Type = ?; SET @Name = ?; SET @Region = ?; SET @Venue_ID = ?; SET @Round_Number = ?; SET @Triple_Crown = ?; SET @Gate_Drop = ?; SET @Whoop_Section = ?; SET @Sand_Section = ?; SET @Soil_Id = ?; CALL Add_or_Update_Event(@Id, @Ama_id, @Type, @Name, @Region, @Venue_ID, @Round_Number, @Triple_Crown, @Gate_Drop, @Whoop_Section, @Sand_Section, @Soil_Id);";
+  
+  mysqlConnection.query(
+    sql,
+    [
+      event.Id,
+      event.Ama_id,
+      event.Type,
+      event.Name,
+      event.Region,
+      event.Venue_ID,
+      event.Round_Number,
+      event.Triple_Crown,
+      event.Gate_Drop,
+      event.Whoop_Section,
+      event.Sand_Section,
+      event.Soil_Id,
+    ],
+    (err, results, fields) => {
+      if (!err) {
+        res.send(
+          "The data for the selected event has been successfully updated."
+        );
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+Router.delete("/:Id", (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM Event WHERE Id = ?",
+    [req.params.Id],
+    (err, results,fields)=> {
+      if(!err){
+        res.send("The selected event has successfully been deleted.");
+      }else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+const mysql = require ('mysql');
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const eventRoutes = require('./routes/event');
 const path = require('path');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req,res)=>{
-  res.render('home')
-});
+
+app.use(bodyParser.json());
+
+app.use(eventRoutes);
 
 app.listen(3000, ()=>{
   console.log('Serving on port 3000')

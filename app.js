7730e6e435aaf47db76142cf206ba284aa7fd8a8
Dan Bax <Dan.D.Bax@gmail.com>
2023-03-04 08:55:06 -0600

    last commit before Sequelize Branch

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const createServer = require( "./utils/server");
+const createServer = require('./utils/server');
 // const mysqlConnection = require('./utils/database');
 
 const app = createServer();
 
-
 // The app.listen() function is used to bind and listen the connections on the
 // specified host and port. This method is identical to Node’s http.Server.listen() method.
 // https://www.geeksforgeeks.org/express-js-app-listen-function/
-app.listen(3000, ()=>{
-  console.log('Serving on port 3000')
+app.listen(3000, () => {
+  console.log('Serving on port 3000');
 });
+
+app.listen(3000,()=> {});
 
 // To prevent connection timeout, the below function sends a query to
 // mySQL Server every 5 seconds:
